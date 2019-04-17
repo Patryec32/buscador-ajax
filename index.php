d8010dca72872664fb3b1b855c4be15c63123238
patry <patryec30@gmail.com>
2019-04-17 20:23:25 -0300

    inicio del buscador

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Buscador Ajax y jQuary</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+	<script src="js/jquery-3.4.0.min.js"></script>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<div class="container">
+		<div class="center">
+			<img src="img/opcion6.jpg" alt="logo">
+		</div>
+		<div class="form center">
+			<form action="" method="post" name="search_form" id="search_form">
+				<input type="text" name="search" id="search">
+			</form>
+		</div>
+
+	<div id="resultados"></div>
+	<div class="footer center">
+		Copyright 2014 - Reservados todos los derechos
+	  <a href="">www.goo.com</a>
+	</div>
+	
+	</div>
+</body>
+</html>
